5be8728f5be79a87ccbb22550d5a7ea639e12b54
mo3g89 <46707248+mo3g89@users.noreply.github.com>
2019-01-16 15:39:30 +0200

    Update wp-install.sh

--- shell-scripts/wp-install.sh
+++ shell-scripts/wp-install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "${SERVER_WEBROOT}/wp-content" ]; then
+    echo "Directory ${SERVER_WEBROOT}/wp-content not exist, please check nfs mount."
+    exit 1
+fi
+
 if [ -z "$(ls -A ${SERVER_WEBROOT}/wp-content)" ]; then 
     wget -qO- 'https://wordpress.org/latest.tar.gz' | tar xz -C /tmp
     mv /tmp/wordpress/* ${SERVER_WEBROOT}
