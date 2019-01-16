cca111b1287cc2763f2ca97774884ce32f06feef
mo3g89 <46707248+mo3g89@users.noreply.github.com>
2019-01-16 18:44:23 +0200

    Update wp-install.sh

--- shell-scripts/wp-install.sh
+++ shell-scripts/wp-install.sh
@@ -7,10 +7,13 @@ fi
 
 if [ -z "$(ls -A ${SERVER_WEBROOT}/wp-content)" ]; then 
     wget -qO- 'https://wordpress.org/latest.tar.gz' | tar xz -C /tmp
+    chown -R litespeed:litespeed /tmp/wordpress
+    mv /tmp/wordpress/wp-content /tmp/
     mv /tmp/wordpress/* ${SERVER_WEBROOT}
+    mv /tmp/wp-content/* ${SERVER_WEBROOT}/wp-content/
 else
     wget -qO- 'https://wordpress.org/latest.tar.gz' | tar xz -C /tmp 
     rm -rf /tmp/wordpress/wp-content 
+    chown -R litespeed:litespeed /tmp/wordpress
     mv /tmp/wordpress/* ${SERVER_WEBROOT}
 fi
-chown -R litespeed:litespeed ${SERVER_WEBROOT}
